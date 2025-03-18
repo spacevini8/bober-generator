5cab2e51355001d1c9300d3303e355d6f1fb0f62
spacevini8 <vinicio.caravana@gmail.com>
2025-03-18 17:09:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+print("#################################################")
+print("Welcome to kurwa animal generator extravaganza!")
+print("v0.1α")
+print("In loving memory of my sanity")
+print("Thanks to Daniel for the idea")
+print("Thanks to Leonard for the names")
+print("#################################################")
+print("")
+print("THE ANIMAL IS:")
+print("")
+
+kurwa_mode = False
+
+# Temporary name list, waiting for Leonard to provide the real ones
+# Do your homework quicker bruh
+
+animal_names=('Bóbr','Ratatuj','Skunks')
+
+selected_animal = random.choice(animal_names)
+
+if kurwa_mode == True:
+    print("kurwa " + selected_animal + "!")
+elif kurwa_mode == False:
+    print(selected_animal)
