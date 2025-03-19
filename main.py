b4140681600a904723b7a36940d632d2b705f6bd
spacevini8 <vinicio.caravana@gmail.com>
2025-03-19 21:43:01 +0100

    re-implements "Kurwa mode" (sorry leonardo)

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import random
 
 print("#################################################")
 print("Welcome to polish animal generator extravaganza!")
-print("v0.2α")
+print("v0.10α")
 print("In loving memory of my sanity")
 print("Thanks to Daniel for the idea")
 print("Thanks to Leonard for the names")
 print("#################################################")
 
+kurwa_select = input("Kurwa mode (Y/n)? ")
+
 print("")
 print("THE ANIMAL IS:")
 print("")
+
+kurwa_mode = False
+
+if kurwa_select == "Y" or kurwa_select == "y":
+    kurwa_mode = True
+elif kurwa_select == "N" or kurwa_select == "n" or kurwa_select == "":
+    kurwa_mode = False
 
 while True:
 
@@ -35,8 +44,12 @@ while True:
 
     selected_animal = random.choice(animal_names)
 
-    print(selected_animal)
-    print("")
+    if kurwa_mode == True:
+        print("kurwa " + selected_animal + "!")
+        print("")
+    elif kurwa_mode == False:
+        print(selected_animal)
+        print("")
 
     again = input("Again (Y/n)? ")
 
@@ -44,4 +57,7 @@ while True:
         print("")
         continue
     elif again == "N" or again == "n":
+        break
+    else:
+        print("Invalid input, exiting")
         break
